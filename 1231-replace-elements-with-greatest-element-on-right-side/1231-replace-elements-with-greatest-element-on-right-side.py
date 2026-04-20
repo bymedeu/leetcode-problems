ed555bda42d8483e11132baff95416930a07384b
Medèu <47332436+bymedeu@users.noreply.github.com>
2026-04-20 16:15:46 +0200

    1231-REPLACE-ELEMENTS-WITH-GREATEST-ELEMENT-ON-RIGHT-SIDE Stats: Time: 17 ms (81.86%), Space: 20.6 MB (34.29%) - LeetHub

--- 1231-replace-elements-with-greatest-element-on-right-side/1231-replace-elements-with-greatest-element-on-right-side.py
+++ 1231-replace-elements-with-greatest-element-on-right-side/1231-replace-elements-with-greatest-element-on-right-side.py
@@ -1,24 +1,9 @@
 class Solution:
-    def getMaxIndex(self, start: int, arr: List[int]):
-        lenA = len(arr)
-        maxx = start + 1
-        for i in range(start + 1, lenA):
-            if arr[i] > arr[maxx]:
-                maxx = i
-        return maxx
-
     def replaceElements(self, arr: List[int]) -> List[int]:
-        i = 0
-        lenA = len(arr)
-        res = []
-        while i < lenA - 1:
-            maxId = self.getMaxIndex(i, arr)
-            while i < maxId:
-                res.append(arr[maxId])
-                i += 1
-                print("appended at ", i)
-            print(i, maxId)
-        res.append(-1)
-        return res
+        maxx = -1
+        lA = len(arr)
+        for i in range(lA - 1,  -1, -1):
+            arr[i], maxx = maxx, max(maxx, arr[i])
+        return arr
 
         
